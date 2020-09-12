230c664be59fce7481188334ece45e0f935e5d6e
Ayush Kumar <kumarayush731@gmail.com>
2020-09-12 16:25:09 +0530

    like schema created

---
+++ models/likes.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const LikeSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      require: [true, 'Like must belongs to the user'],
+    },
+    likes: {
+      type: mongoose.Schema.ObjectId,
+      refPath: 'onModel',
+      required: [true, 'Likes must belong to the Post or Comment'],
+    },
+    onModel: {
+      type: String,
+      required: [true, 'please provide onModel'],
+      enum: ['Post', 'Comment'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const like = mongoose.model('Like', LikeSchema);
+module.exports = like;
