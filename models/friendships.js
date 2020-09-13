1f9e31010b4034768d5032c452d4bd5c8978aef0
Ayush Kumar <kumarayush731@gmail.com>
2020-09-13 12:16:26 +0530

    create friendhip schema

---
+++ models/friendships.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const FriendSchema = new Schema(
+  {
+    from_user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'from_user must belongs to current user'],
+    },
+    to_user: {
+      type: mongodb.Schema.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const friend = mongoose.model('Friend', FriendSchema);
+module.exports = friend;
