0a5b6413222c13180437fb00f5ed11cde3efe9e8
Ayush Kumar <kumarayush731@gmail.com>
2020-09-13 20:26:08 +0530

    bug fix

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -18,10 +18,15 @@ passport.use(
   new JWTStrategy(opts, function (jwtPayload, done) {
     User.findById(jwtPayload._id, function (err, user) {
       //when user changed password then he/she can't be used old token after changed the password
-      if (user.updatedAt) {
-        const changedTimeStamp = parseInt(user.updatedAt.getTime() / 1000, 10);
-        if (jwtPayload.iat < changedTimeStamp) {
-          return done(null, false);
+      if (user) {
+        if (user.updatedAt) {
+          const changedTimeStamp = parseInt(
+            user.updatedAt.getTime() / 1000,
+            10,
+          );
+          if (jwtPayload.iat < changedTimeStamp) {
+            return done(null, false);
+          }
         }
       }
 
