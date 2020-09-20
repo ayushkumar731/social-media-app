3f2dc8ae4ae713925ee73d2e90d87c012b8b7f9c
ayushkumar731 <kumarayush731@gmail.com>
2020-09-20 13:34:10 +0530

    routes improve

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homecontroller');
+const authController = require('../controllers/api/v1/authController');
 
 router.use('/api', require('./api'));
 router.use('/accounts', require('./accounts'));
 
-router.get('/', homeController.home);
+router.get('/',authController.isLoggedIn, homeController.home);
 
 module.exports = router;
