7df234ad6e886114d7a04e5042a434ebcb09c4cd
ayushkumar731 <kumarayush731@gmail.com>
2020-09-20 13:33:09 +0530

    add image and login js created

---
+++ assets/js/login.js
@@ -0,0 +1,32 @@
+const logInForm = document.getElementById('form-login');
+
+if (logInForm) {
+  logInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.getElementById('log-in').textContent = 'Processing...';
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    await login(email, password);
+    document.getElementById('log-in').textContent = 'Log In';
+  });
+}
+
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/user/create-session',
+      data: {
+        email,
+        password,
+      },
+    });
+    if (res.data.status === 'success') {
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1000);
+    }
+  } catch {
+    console.log('error', err.response.data.message);
+  }
+};
