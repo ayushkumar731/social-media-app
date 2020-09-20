028fc83d933c6b3f15645f44b68902ad4c55946a
ayushkumar731 <kumarayush731@gmail.com>
2020-09-20 10:38:16 +0530

    console added

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -79,6 +79,7 @@ const sendErrorProd = (err, req, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  console.log(err);
 
   if (process.env.NODE_ENV == 'development') {
     sendErrorDev(err, req, res);
