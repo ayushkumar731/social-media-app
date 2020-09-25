5f5dd0944f29780bd56d4d2b57f5ad9c98734dd7
ayushkumar731 <kumarayush731@gmail.com>
2020-09-25 16:15:04 +0530

    update current user password

--- assets/js/usersettings.js
+++ assets/js/usersettings.js
@@ -1,5 +1,8 @@
 {
   const userDataForm = document.getElementById('form-user-data');
+  const userPasswordForm = document.getElementById(
+    'form-current-user-password'
+  );
 
   if (userDataForm) {
     userDataForm.addEventListener('submit', async (e) => {
@@ -30,6 +33,43 @@
         ).src = `/img/users/${res.data.data.user.photo}`;
         alert('Data updated');
       }
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
+
+  if (userPasswordForm) {
+    userPasswordForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      document.getElementById('btn-save-user-password').textContent =
+        'Updating...';
+
+      const currentPassword = document.getElementById('currentPassword').value;
+      const password = document.getElementById('password').value;
+      const confirmPassword = document.getElementById('confirmPassword').value;
+
+      await passwordChange(currentPassword, password, confirmPassword);
+
+      document.getElementById('currentPassword').value = '';
+      document.getElementById('password').value = '';
+      document.getElementById('confirmPassword').value = '';
+      document.getElementById('btn-save-user-password').textContent =
+        'SAVE PASSWORD';
+    });
+  }
+
+  const passwordChange = async (currentPassword, password, confirmPassword) => {
+    try {
+      const res = await axios({
+        method: 'PATCH',
+        url: '/api/v1/user/update-password',
+        data: {
+          currentPassword,
+          password,
+          confirmPassword,
+        },
+      });
+      console.log(res);
     } catch (err) {
       alert(err.response.data.message);
     }
