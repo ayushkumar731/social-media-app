c84022370d0c95bcaf8d333a8162528266603ddc
ayushkumar731 <kumarayush731@gmail.com>
2020-09-26 09:59:27 +0530

    babel done

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: './assets/js/index.js',
+  entry: ['babel-polyfill', './assets/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'assets/js'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
