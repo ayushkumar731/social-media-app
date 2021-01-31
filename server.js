5f963b07ba19c53c5a56a46896851f73576be849
ayushkumar731 <kumarayush731@gmail.com>
2021-01-31 21:27:24 +0530

    temp file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, (err) => {
   if (err) {
